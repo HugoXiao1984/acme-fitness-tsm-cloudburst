013455cc154eda4f6d6faf3bf50ea23c30e0fdb0
ubuntu <ubuntu@ubuntu-219.haas-219.pez.pivotal.io>
2021-04-19 23:25:49 -0700

    initial commit

---
+++ deploy-onprem.sh
@@ -0,0 +1,5 @@
+kubectl apply -f gateway.yaml
+kubectl apply -f secrets.yaml
+
+kubectl apply -f application.yaml
+kubectl apply -f catalog.yaml
---
+++ deploy-remote.sh
@@ -0,0 +1,4 @@
+kubectl apply -f gateway.yaml
+kubectl apply -f secrets.yaml
+
+kubectl apply -f catalog.yaml
